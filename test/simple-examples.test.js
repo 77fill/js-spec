a0509aedf23a760a7a8bbf31bb9ea8329aa3a48f
Philipp Schmalz <77fill@googlemail.com>
2025-05-17 20:01:07 +0200

    Make ns["age"] === "ns/age"
    Instead of ns["ns/age"] === "ns/age"
    Add regex

--- test/simple-examples.test.js
+++ test/simple-examples.test.js
@@ -3,19 +3,23 @@ import {isValid} from "../lib/isValid.js";
 import {describe, it, expect} from "vitest";
 import {$and, $not} from "../lib/spec.js";
 
-const basic = $def({
-    "basic/word": $and(isString, /[a-zA-Zäöüß]+/.test)
+// Specs:
+
+const basic = $def("basic",{
+    "word": $and(isString, /[a-zA-Zäöüß]+/.test)
 })
 
-const domain = $def({
-    "domain/firstname": basic["basic/word"],
-    "domain/age": $and(isInteger, notNaN, $not(isNegative)),
+const domain = $def("domain",{
+    "firstname": basic.word,
+    "age": $and(isInteger, notNaN, $not(isNegative)),
 })
+
+// Behavior:
 
 describe("simple examples", () => {
     it("should greenlight all datastructures of all types", () => {
         expect(isValid({
-            "domain/name": "Philipp",
+            [domain.firstname]: "Philipp",
             "domain/age": 28,
         }))
             .to.be.true
