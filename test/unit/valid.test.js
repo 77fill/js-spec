d6a5cb035439542bd3633ec618a45f39c3f299f1
Philipp Schmalz <77fill@googlemail.com>
2025-06-09 14:34:15 +0200

    Implement polymorphic $valid

---
+++ test/unit/valid.test.js
@@ -0,0 +1,35 @@
+import { expect } from "chai"
+import { $def, $valid, isArray, isInteger, isString } from "../../lib/main"
+
+$def("domain", {
+    firstname: isString
+})
+
+describe("VALID", () => {
+    it("validates a map entity", () => {
+        expect(
+            $valid({
+                "domain/firstname": "Tom"
+            })
+        )
+        .to.be.true
+    })
+
+    it("validates a value with a spec-like", () => {
+        expect(
+            $valid(isInteger, -4)
+        )
+        .to.be.true
+    })
+
+    it("validates several values with spec-likes", () => {
+        expect(
+            $valid(
+                [isArray, []],
+                [a => a > 3, 5],
+                [true, true]
+            )
+        )
+        .to.be.true
+    })
+})
