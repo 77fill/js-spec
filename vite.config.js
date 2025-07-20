0090b4cd3e1e4b536b303214a8a2f951e78f8176
Philipp Schmalz <77fill@googlemail.com>
2025-07-20 14:34:28 +0200

    Configure project to use '@/*' style absolute paths

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import path from 'path'
+import path, { resolve } from 'path'
 import {defineConfig} from 'vite'
 
 export default defineConfig({
@@ -7,6 +7,11 @@ export default defineConfig({
             entry: path.resolve(__dirname, 'lib/main.js'),
             name: 'js-spec',
             fileName: (format, entryName) => `${entryName}.${format}.js`
+        }
+    },
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, '.')
         }
     }
 });
